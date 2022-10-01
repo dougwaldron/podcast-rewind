f67465a0a4745375b13a78f43c9ff5300563aa1e
Doug Waldron <doug@aqueo.us>
2022-09-30 20:38:20 -0400

    Fix the Apple Podcasts subscribe button

--- PodcastRewind/Pages/Details.cshtml.cs
+++ PodcastRewind/Pages/Details.cshtml.cs
@@ -10,6 +10,7 @@ public class DetailsModel : PageModel
 {
     public FeedRewindData FeedRewindData { get; private set; } = null!;
     public string RewindFeedUrl { get; private set; } = string.Empty;
+    public string ApplePodcastSubscribeUrl { get; private set; } = string.Empty;
     public Guid RewindFeedId { get; private set; }
     public SyndicationFeed? RewoundFeed { get; private set; }
     public SyndicationFeed? ScheduledFeed { get; private set; }
@@ -31,6 +32,7 @@ public class DetailsModel : PageModel
         ScheduledFeed = await FeedRewindData.GetScheduledFeedAsync();
         RewindFeedId = id.Value;
         RewindFeedUrl = Url.ActionLink("GetFeed", "Feed", new { id })!;
+        ApplePodcastSubscribeUrl = Url.ActionLink("GetFeed", "Feed", new { id }, "podcast")!;
         
         return Page();
     }
