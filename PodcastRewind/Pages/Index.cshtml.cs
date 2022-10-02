b5899d1a05487207fbcf34383968de5dfd121f9a
Doug Waldron <doug@aqueo.us>
2022-10-02 12:34:04 -0400

    Add index page and format the logo

--- PodcastRewind/Pages/Index.cshtml.cs
+++ PodcastRewind/Pages/Index.cshtml.cs
@@ -1,46 +1,11 @@
-﻿using iTunesPodcastFinder;
-using iTunesPodcastFinder.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PodcastRewind.Pages;
 
 public class IndexModel : PageModel
 {
-    [BindProperty]
-    [Display(Name = "Search for:")]
-    public string? Search { get; set; }
-
-    public List<Podcast> SearchResults { get; private set; } = new();
-
-    [BindProperty]
-    [Display(Name = "Enter Feed URL")]
-    public string? FeedUrl { get; set; }
-
-    private readonly PodcastFinder _finder;
-    public IndexModel() => _finder = new PodcastFinder();
-
-    public async Task<IActionResult> OnGetAsync(string? search)
+    public void OnGet()
     {
-        if (search is null) return Page();
-        return await GetAsyncInternal(search);
-    }
-
-    public async Task<IActionResult> GetAsyncInternal(string search)
-    {
-        // Default: retrieve 30 items using the US search
-        try
-        {
-            SearchResults = (await _finder.SearchPodcastsAsync(search, 30))
-                .Where(p => p.FeedUrl is not null).ToList();
-        }
-        catch (HttpRequestException)
-        {
-            ModelState.AddModelError("Search",
-                "The iTunes podcast search service did not respond.");
-        }
-
-        return Page();
+        // Nothing to see here.
     }
 }
---
+++ PodcastRewind/Pages/Search.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using iTunesPodcastFinder;
+using iTunesPodcastFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace PodcastRewind.Pages;
+
+public class SearchModel : PageModel
+{
+    [BindProperty]
+    [Display(Name = "Search for:")]
+    public string? Search { get; set; }
+
+    public List<Podcast> SearchResults { get; private set; } = new();
+
+    [BindProperty]
+    [Display(Name = "Enter Feed URL")]
+    public string? FeedUrl { get; set; }
+
+    private readonly PodcastFinder _finder;
+    public SearchModel() => _finder = new PodcastFinder();
+
+    public async Task<IActionResult> OnGetAsync(string? search)
+    {
+        if (search is null) return Page();
+        return await GetAsyncInternal(search);
+    }
+
+    public async Task<IActionResult> GetAsyncInternal(string search)
+    {
+        // Default: retrieve 30 items using the US search
+        try
+        {
+            SearchResults = (await _finder.SearchPodcastsAsync(search, 30))
+                .Where(p => p.FeedUrl is not null).ToList();
+        }
+        catch (HttpRequestException)
+        {
+            ModelState.AddModelError("Search",
+                "The iTunes podcast search service did not respond.");
+        }
+
+        return Page();
+    }
+}
