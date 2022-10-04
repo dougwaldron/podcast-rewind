f6fe90539d216bd91998af1bf00b8084340212a7
Doug Waldron <doug@aqueo.us>
2022-10-03 21:53:42 -0400

    Add unit tests project
    ref #44

---
+++ PodcastRewind.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PodcastRewind.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
